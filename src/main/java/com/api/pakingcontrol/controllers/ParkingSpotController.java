932fb3f447e730475c7640b88643aeb41bbe2141
lgshogun <lauro.gpereira@gmail.com>
2023-01-23 19:16:29 -0300

    :sparkles: FEAT: Criado o método POST

--- src/main/java/com/api/pakingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/pakingcontrol/controllers/ParkingSpotController.java
@@ -1,9 +1,16 @@
 package com.api.pakingcontrol.controllers;
 
+import com.api.pakingcontrol.dtos.ParkingSpotDto;
+import com.api.pakingcontrol.models.ParkingSpotModel;
 import com.api.pakingcontrol.services.ParkingSpotService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins="*",maxAge = 3600)
@@ -13,5 +20,12 @@ public class ParkingSpotController {
 
     public ParkingSpotController(ParkingSpotService parkingSpotService) {
         this.parkingSpotService = parkingSpotService;
+    }
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto){
+        var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
     }
 }
--- src/main/java/com/api/pakingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/pakingcontrol/services/ParkingSpotService.java
@@ -1,7 +1,9 @@
 package com.api.pakingcontrol.services;
 
 
+import com.api.pakingcontrol.models.ParkingSpotModel;
 import com.api.pakingcontrol.repositories.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +13,10 @@ public class ParkingSpotService {
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
     }
 }
